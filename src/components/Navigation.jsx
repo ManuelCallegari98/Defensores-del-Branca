1f1f4244cf62f69ed24493508b02412ba7f97638
ManuelCallegari98 <manuelcallegari98@gmail.com>
2024-01-03 12:42:45 -0300

    Cambios en Barra de navegacion y Shop
    Primera Version

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,19 +6,19 @@ const Navigation = () => {
         <header className="text-white body-font bg-[#870A28]">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-                    <Link to="/home" className="mr-5 hover:text-[#00613c]">First Link</Link>
-                    <Link to="/shop" className="mr-5 hover:text-[#00613c]">Second Link</Link>
-                    <Link to="/team" className="mr-5 hover:text-[#00613c]">Third Link</Link>
-                    <Link to="/about" className="hover:text-[#00613c]">Fourth Link</Link>
+                    <Link to="/home" className="mr-5 hover:text-[#00613c]">Home</Link>
+                    <Link to="/shop" className="mr-5 hover:text-[#00613c]">Shop</Link>
+                    <Link to="/team" className="mr-5 hover:text-[#00613c]">Team</Link>
+                    <Link to="/about" className="hover:text-[#00613c]">About</Link>
                 </nav>
                 <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                     </svg>
-                    <span className="ml-3 text-xl text-white">Tailblocks</span>
+                    <span className="ml-3 text-l text-white">Defensores Del Branca</span>
                 </a>
                 <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-                    <button className="inline-flex items-center bg-[#00613c] border-0 py-1 px-3 focus:outline-none hover:bg-[#00613cae] rounded text-base mt-4 md:mt-0">Button
+                    <button className="inline-flex items-center bg-[#00613c] border-0 py-1 px-3 focus:outline-none hover:bg-[#00613cae] rounded text-base mt-4 md:mt-0">Ingresar
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                         </svg>

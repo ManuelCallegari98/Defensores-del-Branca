a69e4d2c22672cdef608d04e0bf259eb830a66bb
ManuelCallegari98 <manuelcallegari98@gmail.com>
2024-01-18 00:29:23 -0300

    v2.1

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -7,13 +7,13 @@ const History = () => {
             <h2 className="text-2xl font-bold ">Nuestra Historia</h2>
             <div className="p-1 gap-2 flex flex-col md:flex-row items-center justify-center pt-12 ">
                 <div>
-                    <p className="text-gray-700 text-center ">
+                    <p className="text-black text-center ">
                         ¡Bienvenidos a la historia apasionante de los Defensores del Branca, un equipo forjado en la pasión del fútbol argentino y el compañerismo inquebrantable! Todo comenzó un 5 de abril de 2018, cuando un grupo de amigos decidieron dar vida a un sueño compartido: formar un equipo que trascendiera más allá del juego en la cancha.
                     </p>
-                    <p className="text-gray-700 text-center">
+                    <p className="text-black text-center">
                         Ubicados en el corazón de Concepción del Uruguay, nuestro hogar inicialmente se encontraba en el complejo cercano a la facultad, en la emblemática 3 de febrero 740. En esos primeros días, el equipo carecía de colores definidos y un escudo distintivo; sin embargo, la pasión, la amistad y el amor por el Branca eran los verdaderos emblemas que nos unían en cada partido.
                     </p>
-                    <p className="text-gray-700 text-center">
+                    <p className="text-black text-center">
                         En nuestros primeros encuentros, el "Defe" del Branca se destacaba con camisetas de color violeta, camisetas que eran prestadas desde la facultad. Con cada partido, forjábamos no solo nuestra identidad en la cancha, sino también la conexión especial que nos unía como amigos y compañeros de equipo.
                     </p>
                 </div>
@@ -22,7 +22,7 @@ const History = () => {
             <div className="p-1 gap-2 flex flex-col md:flex-row items-center justify-center pt-12">
                 <img src="https://i.ibb.co/LPYc6Ys/2018DDB.jpg" alt="DDB en primer año ya con camiseta y colores propios" className="h-[60vh] md:order-first" />
                 <div>
-                    <p className="text-gray-700 text-center">
+                    <p className="text-black text-center">
                         El primer año de competición fue todo un desafío, pero demostramos desde el principio que estábamos destinados a grandes logros. Alcanzamos el subcampeonato, marcando así el inicio de una viaje lleno de emociones y desafíos. Sin embargo, la llegada del covid-19 y la transición a la virtualidad mantuvo a las águilas alejadas de las canchas durante un tiempo.
                     </p>
                 </div>
@@ -30,13 +30,13 @@ const History = () => {
 
             <div className="p-1 gap-2 flex flex-col md:flex-row items-center justify-center pt-4">
                 <div>
-                    <p className="text-gray-700 text-center">
+                    <p className="text-black text-center">
                         Una vez termino la pandemia, en el año 2022, el Defe volvio a las canchas. En ese torneo, conquistamos un destacado cuarto puesto, demostrando que nuestro compromiso y dedicación estaban más fuertes que nunca. El 2023 nos llevó aún más lejos: clasificamos a los playoffs, llegando a las semifinales, donde lamentablemente caímos en los penales.
                     </p>
-                    <p className="text-gray-700 text-center">
+                    <p className="text-black text-center">
                         A lo largo de estos años, hemos forjado más que un equipo; hemos construido una familia unida por la pasión por el fútbol y el orgullo de representar a los Defensores del Branca. Cada partido, cada derrota y cada victoria se han convertido en piezas fundamentales de nuestra historia, marcando el camino de un equipo que no solo defiende el honor en el campo, sino que también celebra la amistad y el amor por el Fernet Branca
                     </p>
-                    <p className="text-gray-700 text-center">
+                    <p className="text-black text-center">
                         ¡Acompáñanos en este viaje lleno de pasión, donde se disfruta la escensia de lo argentino: la amistad, el futbol y el fernet Branca!
                     </p>
                 </div>
